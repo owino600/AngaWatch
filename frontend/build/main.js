b044c1ed6c58e68e464d0a0a6cbf6d3c06de396b
owino600 <owinodaniel600@gmail.com>
2025-12-21 17:20:16 +0300

    updates

--- frontend/build/main.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { app as App } from "./app.js";
-var root = createRoot(document.getElementById("root"));
-root.render(/*#__PURE__*/React.createElement(App, null));
